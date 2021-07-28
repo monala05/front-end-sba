6c7f2bfa9922a7b21916efdd2db7986bf9b1a0d5
Alan <alandmontes2@hotmail.com>
2021-07-27 21:04:47 -0700

    Finished the rough draft of reviews page
    A very rough draft of the review page. Fixed some of the styling issues in the index.html page, and the profile.html page. Included js in this page.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # front-end-sba
 Per scholas skill based assessment for front-end skills. 
+
+Video game review website. I will be using this and polishing it for my case study.
+
+three html pages that navigate between each other. Three style sheets for each html page.
+
+One javascript file that is used on the reviews.html page
+
+This website uses dummy data, but i plan to use RAWG api to populate with data
+
+This project uses fa fa font awesome for icons, bootstrap for styling and google fonts.
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,55 @@
-  
+function pop() {
+        var table = document.getElementById("myTable");
 
-function msg(){  
-    alert("Hello Javatpoint");  
-   }  
+
+
+        for(let i = 0; i <20; i++){
+            var row = table.insertRow(-1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+
+            if(i % 4 === 0){
+            cell1.innerHTML = "A cool game";
+            cell2.innerHTML = "Xbox";
+            cell3.innerHTML = "80";
+            cell4.innerHTML = "90";
+            cell5.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing eli sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+            }
+
+            if(i % 4 === 1){
+                cell1.innerHTML = "Dummy game";
+                cell2.innerHTML = "Playstation";
+                cell3.innerHTML = "60";
+                cell4.innerHTML = "40";
+                cell5.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing eli sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+            }
+
+            if(i % 4 === 2){
+                cell1.innerHTML = "Faux data";
+                cell2.innerHTML = "Switch";
+                cell3.innerHTML = "99";
+                cell4.innerHTML = "100";
+                cell5.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing eli sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+            }
+
+            if(i % 4 === 3){
+                cell1.innerHTML = "More dummy data";
+                cell2.innerHTML = "Computer";
+                cell3.innerHTML = "20";
+                cell4.innerHTML = "20";
+                cell5.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing eli sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+                }
+
+        }
+
+}
+
+function submit(){
+    document.getElementById("leave-review").innerHTML = "Thank you"
+}
+
+pop();
+
